96d6cbc1e5d12cacd85f10a8ecbbadb096007cec
Dynnwa <ddaniel.wei@gmail.com>
2022-02-25 15:36:08 -0800

    changed some basics about the site

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteTitle: 'Danny Wei', // <title>
+  manifestName: 'Overflow',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-overflow/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Danny Wei',
+  heading: 'Having Fun',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'Linked-in',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+};
