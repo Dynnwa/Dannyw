b8f3da0b0cac7538e00b73598bf2f3710a28df01
Danny Wei <79378020+Dynnwa@users.noreply.github.com>
2022-03-26 17:36:44 -0700

    Delete .cache directory

--- .cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": () => import("./../../caches/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---cache-caches-gatsby-plugin-offline-app-shell-js" */),
-  "component---src-pages-activities-js": () => import("./../../../src/pages/Activities.js" /* webpackChunkName: "component---src-pages-activities-js" */),
-  "component---src-pages-experience-js": () => import("./../../../src/pages/Experience.js" /* webpackChunkName: "component---src-pages-experience-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-projects-js": () => import("./../../../src/pages/Projects.js" /* webpackChunkName: "component---src-pages-projects-js" */)
-}
-
